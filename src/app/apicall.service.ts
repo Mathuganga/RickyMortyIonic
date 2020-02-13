d19230f61c1fe9480103fc56662f1a446e094a44
mathuganga <mathugangagmail.com@mathugangas-MacBook-Air.local>
2020-02-13 22:13:14 +0530

    comman lines and Css fixes

--- src/app/apicall.service.ts
+++ src/app/apicall.service.ts
@@ -1,34 +1,41 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { HttpClient , HttpResponse} from '@angular/common/http';
+import { Observable, from, throwError } from 'rxjs';
+import { switchMap, tap, map, catchError, mergeMap } from 'rxjs/operators';
+import { forkJoin } from 'rxjs';
+import { element } from 'protractor';
 @Injectable({
   providedIn: 'root'
 })
 export class ApicallService {
-
+  homeworld: any;
+array = [];
   constructor(private http: HttpClient) { }
   getCharacters(urlToFetchData?: string): Observable<IBaseResponse> {
   let url;
-    if (urlToFetchData) {
+  if (urlToFetchData) {
       url = urlToFetchData;
     }
-    return this.http
+  return this.http
       .get<IBaseResponse>(url)
       .pipe(
         tap(
-          characters => console.log(`fetched characters`),
+          characters => console.log(),
           error => console.log(`error -> ${error}`)
         )
       );
   }
-}
+  }
+
 
 export interface IBaseResponse {
-  info: string;
+  info: Info;
   results: [];
 }
 
+// tslint:disable-next-line: no-empty-interface
+export interface T {
+}
 export class Info {
   count: number;
   pages: number;
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,52 +1,59 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ApicallService } from '../apicall.service';
 import { IonInfiniteScroll } from '@ionic/angular';
+import { ThrowStmt } from '@angular/compiler';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
-export class HomePage implements OnInit{
-errorflag:any
+export class HomePage implements OnInit {
+  errorflag: any;
   totalResults: any;
-  infiniteScrollEnabled=true;
-  listarray=[];
-  url  ='https://rickandmortyapi.com/api/character/';
+  infiniteScrollEnabled = true;
+  listarray = [];
+  // initial url as provided in api documents to fetch all characters
+  url = 'https://rickandmortyapi.com/api/character/';
   urlnext: any;
-  @ViewChild(IonInfiniteScroll,{static:false}) infiniteScroll: IonInfiniteScroll;
-  constructor(public apiservice:ApicallService) {
+  @ViewChild(IonInfiniteScroll, { static: false })
+  infiniteScroll: IonInfiniteScroll;
+  locationarray: any[];
+  constructor(public apiservice: ApicallService) {
     this.listarray = [];
-
+    this.locationarray = [];
   }
-ngOnInit()
-{
-  // let data=this.apiservice.getCharacters();
- this.FetchAllList(this.url);
-} 
-  loadData(event)
-  {
-setTimeout(() => { 
-  event.target.complete();
-  if (this.listarray.length ==this.totalResults ) {
-    event.target.disabled = true;
+  ngOnInit() {
+  // To load the data initially when the app is loaded
+    this.FetchAllList(this.url);
   }
-  else
-  {
-    this.FetchAllList(this.urlnext);
-  }
-}, 500);
+  loadData(event) {
+    setTimeout(() => {
+      event.target.complete();
+      // if the array length have been reached the total length the scroll will be disabled
+      if (this.listarray.length === this.totalResults) {
+        event.target.disabled = true;
+      } else {
+        // If not again the api is called from the urlnex which will be provided in the response
+        this.FetchAllList(this.urlnext);
+      }
+    }, 500);
   }
 
-  FetchAllList(url)
-  {
-    this.apiservice.getCharacters(url).subscribe(response => {
-      this.totalResults=response.info['count']
-      this.listarray =[...this.listarray,...response.results];
-      this.urlnext =response.info['next'];
-    },
-    err => {
-this.errorflag =true;
-    },
-    () => console.log('HTTP request completed.'));
+    // To get the charecters from using RickMorty API
+  FetchAllList(url) {
+    this.locationarray = [];
+    this.apiservice.getCharacters(url).subscribe(
+      response => {
+        this.totalResults = response.info.count;
+        this.listarray = [...this.listarray, ...response.results];
+        this.urlnext = response.info.next;
+      },
+      err => {
+        this.errorflag = true;
+        console.log('er', err);
+      },
+      () => {
+      }
+    );
   }
 }
